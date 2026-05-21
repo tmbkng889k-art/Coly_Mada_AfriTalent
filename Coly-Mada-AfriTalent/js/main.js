a9b5166889b6931215466d860a24884c6aca3e7a
Madaa <Madacoly10@icloud.com>
2026-05-21 00:10:49 +0000

    structure complète des 5 pages avec sections et contenu texte

---
+++ Coly-Mada-AfriTalent/js/main.js
@@ -0,0 +1,8 @@
+// window.addEventListener('scroll', function() {
+//     const navbar = document.querySelector('.fixed-top');
+//     if (window.scrollY > 50) { //SI scroll superieur à 50pixel
+//         navbar.classList.add('scrolled');//On ajoute une cless CSS pour le style
+//     } else {
+//         navbar.classList.remove('scrolled');//On retire la class
+//     }
+// });
